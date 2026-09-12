7a049227ffc8cc7ea587726f8ac24113ca4c70cf
aamubaroq85-crypto <aamubaroq85@gmail.com>
2026-09-12 18:02:20 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tlnl",
+    name="tensor-lattice-neural-layer",
     version="0.1.0",
     author="Baroq",
     author_email="aamubaroq85@gmail.com",
